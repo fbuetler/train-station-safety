1c626d761913b570ddfb10ad96202522fc92cf7e
Florian Buetler <flbuetle@ethz.ch>
2020-04-17 03:03:20 +0200

    find all TrainStationInitializer

--- analysis/src/main/java/ch/ethz/rse/pointer/PointsToInitializer.java
+++ analysis/src/main/java/ch/ethz/rse/pointer/PointsToInitializer.java
@@ -21,6 +21,7 @@ import soot.Value;
 import soot.jimple.IntConstant;
 import soot.jimple.internal.JInvokeStmt;
 import soot.jimple.internal.JSpecialInvokeExpr;
+import soot.jimple.internal.JimpleLocal;
 import soot.jimple.spark.pag.Node;
 
 public class PointsToInitializer {
@@ -56,6 +57,7 @@ public class PointsToInitializer {
 	}
 
 	private void analyzeAllInitializers() {
+		int uniqueNumber = 0;
 		for (SootMethod method : this.c.getMethods()) {
 
 			if (method.getName().contains("<init>")) {
@@ -63,10 +65,48 @@ public class PointsToInitializer {
 				continue;
 			}
 
-			// populate data structures
-			// FILL THIS OUT
+			for (Unit s : method.getActiveBody().getUnits()) {
+				/*
+				 * we assume here that there is only one kind of constructor call namely:
+				 * 'TrainStation s = new TrainStation(10)' in Jimple the above statement looks
+				 * like: $r2 = new ch.ethz.rse.TrainStation specialinvoke
+				 * $r2.<ch.ethz.rse.TrainStation: void <init>(int)>(10) The following code
+				 * extracts r2 as Local base and 10 as int nTracks
+				 */
+				if ((s instanceof JInvokeStmt) && ((JInvokeStmt) s).getInvokeExpr() instanceof JSpecialInvokeExpr) {
+					// s is a constructor
+					JInvokeStmt is = (JInvokeStmt) s;
+					JSpecialInvokeExpr sie = (JSpecialInvokeExpr) is.getInvokeExpr();
+
+					if (sie.getMethodRef().getDeclaringClass().getName() == Constants.trainStationClassName) {
+						/*
+						 * we know its a JimpleLocal because this is given as part of the project
+						 * description: assignment only to local variables
+						 */
+						Local base = (JimpleLocal) sie.getBase();
+
+						Value arg = sie.getArg(0);
+						if (arg instanceof IntConstant) {
+							IntConstant ic = (IntConstant) arg;
+							int nTracks = ic.value;
+
+							TrainStationInitializer tsi = new TrainStationInitializer(is, uniqueNumber, nTracks);
+							uniqueNumber++;
+
+							perMethod.put(method, tsi);
+
+							Collection<Node> nodes = pointsTo.getNodes(base);
+							for (Node n : nodes) {
+								initializers.put(n, tsi);
+							}
+						}
+					}
+				}
+			}
+
+			// TODO populate data structures. (flbuetle) are the more?
 		}
 	}
 
-	// FILL THIS OUT
+	// TODO FILL THIS OUT. (flbuetle) what should go here?
 }
