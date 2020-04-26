957b8ecace8ddbc16de1a079cbda7e8f54c404b9
Manuel Hässig <mhaessig@student.ethz.ch>
2020-04-26 19:19:39 +0200

    Added unit tests for Numerical Analysis

---
+++ analysis/src/test/java/ch/ethz/rse/numerical/NumericalAnalysisTest.java
@@ -0,0 +1,43 @@
+package ch.ethz.rse.numerical;
+
+import ch.ethz.rse.utils.TestClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+public class NumericalAnalysisTest {
+    private class UnhandledExecutor implements Executable {
+        private boolean raiseException;
+
+        public UnhandledExecutor(boolean raiseException) {
+            this.raiseException = raiseException;
+        }
+
+        @Override
+        public void execute() throws Throwable {
+            Object obj = new Object();
+            NumericalAnalysis.unhandled("task", obj, this.raiseException);
+        }
+    }
+
+    @Test
+    void unhandledRaiseException() {
+        Assertions.assertThrows(UnsupportedOperationException.class, new UnhandledExecutor(true));
+    }
+    @Test
+    void unhandledNoException() {
+        Assertions.assertDoesNotThrow(new UnhandledExecutor(false));
+    }
+
+    @Test
+    void copy() {
+        TestClass tc = new TestClass("ch.ethz.rse.numerical.testcases.SimpleMethod");
+        NumericalAnalysis na = tc.getNumericalAnalysis().get("method");
+        NumericalStateWrapper wExp = na.newInitialFlow();
+        NumericalStateWrapper wMut = na.entryInitialFlow();
+
+        na.copy(wExp, wMut);
+        Assertions.assertEquals(wExp, wMut);
+    }
+
+}
---
+++ analysis/src/test/java/ch/ethz/rse/numerical/testcases/SimpleMethod.java
@@ -0,0 +1,7 @@
+package ch.ethz.rse.numerical.testcases;
+
+public class SimpleMethod {
+    public int method() {
+        return 1 + 1;
+    }
+}
