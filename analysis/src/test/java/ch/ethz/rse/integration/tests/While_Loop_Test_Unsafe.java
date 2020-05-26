2ec7c975d98f1fdfe9b5391ea167d302219299d9
lmeinen <lmeinen@student.ethz.ch>
2020-05-26 14:31:36 +0200

    Fixed incorrect tests

--- analysis/src/test/java/ch/ethz/rse/integration/tests/While_Loop_Test_Unsafe.java
+++ analysis/src/test/java/ch/ethz/rse/integration/tests/While_Loop_Test_Unsafe.java
@@ -3,11 +3,12 @@ package ch.ethz.rse.integration.tests;
 import ch.ethz.rse.TrainStation;
 
 // expected results:
-// TRACK_NON_NEGATIVE UNSAFE
-// TRACK_IN_RANGE UNSAFE
-// NO_CRASH SAFE
+// TRACK_NON_NEGATIVE SAFE
+// TRACK_IN_RANGE SAFE
+// NO_CRASH UNSAFE
 
 public class While_Loop_Test_Unsafe {
+    // See: https://moodle-app2.let.ethz.ch/mod/forum/discuss.php?d=51019
 	public static void m1(int j) {
         TrainStation s = new TrainStation(10);
     
