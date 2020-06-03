a7978511d667d370f22e1f122b0f187275e1238e
Florian Buetler <flbuetle@ethz.ch>
2020-06-03 17:05:37 +0200

    move lasses awesome script to the project root

--- enable.sh
+++ enable.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+PROJECTROOT="$(pwd -P)"
+TESTSPATH="$PROJECTROOT/analysis/src/test/java/ch/ethz/rse/integration/tests"
+
+echo "$TESTSPATH"
+
 cat testSettings.txt | while read line || [[ -n $line ]];
 do
     if [[ "${line: -1}"  == "D" ]]
     then
         read -r filename rest_of_string <<<"$line" 
         echo "$filename"
-        sed -i 's/ENABLED/DISABLED/' "tests/$filename.java"
+        sed -i 's/ENABLED/DISABLED/' "$TESTSPATH/$filename.java"
     else
         read -r filename rest_of_string <<<"$line" 
         echo "$filename"
-        sed -i 's/DISABLED/ENABLED/' "tests/$filename.java"
+        sed -i 's/DISABLED/ENABLED/' "$TESTSPATH/$filename.java"
     fi
 done
